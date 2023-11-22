f4a3b37c720ba78a53a8ced87451a2ce992509fd
kid <2713786149@qq.com>
2023-11-22 20:50:15 +0800

    01-项目初始化

---
+++ src/store/map.ts
@@ -0,0 +1,6 @@
+import {defineStore} from "pinia";
+
+export const useMapStore = defineStore('map', ()=>{
+    const map = [1,2,3]
+    return { map }
+})
---
+++ src/store/tests/map.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+
+describe("map", () => {
+    it('should ', () => {
+        expect(true).toBe(true);
+    });
+})
